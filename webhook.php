46f6afe18c10fc113f10ad8906a70c0256e0ef82
Uochusma <Kyoshino070903@outlook.jp>
2018-04-11 11:28:41 +0900

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,12 +1,12 @@
 <?php
 
 $LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = '** secret **';
+$SECRET_KEY = '**secret**';
 
 if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($payload['ref'] === 'refs/heads/master') {
-        `cd /path/to/repository; git pull origin master`;
+        `pwd; git pull origin master`;
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
